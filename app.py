d3510094caa4357e1b953cd84921a766ea1dad2a
ArchDevData <archijogi64@gmail.com>
2024-11-29 17:10:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from langflow import main
-from langflow.interface.load import load_flow_from_json
+from langflow import load_flow_from_json
 
 if __name__ == "__main__":
     flow_file = "LangRAG.json"
